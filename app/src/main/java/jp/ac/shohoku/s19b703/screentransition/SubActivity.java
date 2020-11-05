051a568a86688e049251d2ee447aba933374ef27
s19B706 <s19b706@shohoku.ac.jp>
2020-11-05 22:22:42 +0900

    できたぁ！！！！

--- app/src/main/java/jp/ac/shohoku/s19b703/screentransition/SubActivity.java
+++ app/src/main/java/jp/ac/shohoku/s19b703/screentransition/SubActivity.java
@@ -16,17 +16,39 @@
  package jp.ac.shohoku.s19b703.screentransition;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /*@auther myu kato*/
 public class SubActivity extends Activity {
    int count = 1;
    @Override
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_sub);
+       // setContentView(R.layout.activity_sub);
+
+       // 課題16
+       LinearLayout linearLayout = new LinearLayout(this);
+       linearLayout.setOrientation(LinearLayout.VERTICAL);
+       setContentView(linearLayout);
+
+       Intent intent = getIntent();
+
+       TextView textview1 = new TextView(this);
+       String str1 = intent.getStringExtra("number");
+       textview1.setText(str1);
+
+       TextView textview2 = new TextView(this);
+       String str2 = intent.getStringExtra("pass");
+       textview2.setText(str2);
+
+       linearLayout.addView(textview1, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+       linearLayout.addView(textview2, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
    }
 }
--- app/src/main/java/jp/ac/shohoku/s19b703/screentransition/TransActivity.java
+++ app/src/main/java/jp/ac/shohoku/s19b703/screentransition/TransActivity.java
@@ -18,10 +18,13 @@
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
 /*@auther myu kato*/
 public class TransActivity extends Activity {
    int count = 1;
@@ -36,6 +39,12 @@ public class TransActivity extends Activity {
            public void onClick(View v) {
                //インテントに、この画面と、遷移する別の画面を指定する
                Intent intent = new Intent(TransActivity.this, SubActivity.class);
+               EditText num = (EditText) findViewById(R.id.editText);
+               Editable numtext = num.getText();
+               EditText pass = (EditText) findViewById(R.id.editText2);
+               Editable passtext = pass.getText();
+               intent.putExtra("number","学籍番号："+numtext);
+               intent.putExtra("pass","パスワード："+passtext);
                //インテントで指定した別の画面に遷移する
                startActivity(intent);
            }
